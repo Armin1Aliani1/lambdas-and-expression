2bc138cce6d34efd9eb1ceaa8d09f6fe79feb57e
Dragon2623 <armin1.aliani1@gmail.com>
2022-07-03 15:51:00 +0430

    edit LambdaApplicationForTestFore

--- lambdasAndStreams/lambdasAndStreams/src/main/java/test4OrForeLambdaPracticeOneUsernameValid/LambdaApplicationForTestFore.java
+++ lambdasAndStreams/lambdasAndStreams/src/main/java/test4OrForeLambdaPracticeOneUsernameValid/LambdaApplicationForTestFore.java
@@ -16,12 +16,19 @@ public class LambdaApplicationForTestFore {
 
     private static void checkForUsernameValid(String strOne) {
         Predicate<String> stringPredicateOne = s -> (s != null && s.length() >= 5 && s.length() <= 10);
-        Predicate<String> stringPredicateTwo = s -> (s.equalsIgnoreCase("Armi"));
+        Predicate<String> stringPredicateTwo = s -> (s.equalsIgnoreCase("Armin"));
 
         boolean validOne = stringPredicateOne.test(strOne);
-        boolean validTwo = stringPredicateOne.and(stringPredicateTwo).test(strOne);
+        Predicate<String> stringPredicateThree = stringPredicateOne.and(stringPredicateTwo);
+        boolean validTwo = stringPredicateThree.test(strOne);
 
         System.out.println("Username valid : " + validOne);
+
+//   Is condition if :  (condition)? "true"                                    :((else))  "false"
+        String result = (validOne) ? "Length username is : " + strOne.length() : "Username should not be empty or less than 5 characters or more than 10 characters";
+
+        System.out.println(result);
+
         System.out.println("Username is available : " + validTwo);
     }
 }
